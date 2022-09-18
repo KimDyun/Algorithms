0389a0d1733b44bcd361d96f958154a051878598
KimDyun <swslooser@naver.com>
2022-09-18 21:31:14 +0900

    DFS_BFS.py first commit

---
+++ Graph Traversal/DFSandBFS/DFS_BFS.py
@@ -0,0 +1,48 @@
+import sys
+
+def graph_loader(N,input):
+    graph = dict()
+
+    for e in input[1:]:
+        if e[0] not in graph:
+            graph[e[0]] = [e[1]]
+            graph[e[1]] = [e[0]]
+        else:
+            graph[e[0]].append(e[1])
+            graph[e[1]].append(e[0])
+
+    return graph
+
+def DFS(graph, start):
+    visited = list()
+    stack = list()
+
+    stack.append(start)
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            stack.extend(graph[node])
+    return visited
+
+def BFS(graph, start):
+    visited = list()
+    queue = list()
+
+    queue.append(start)
+    while queue:
+        node = queue.pop(0)
+        if node not in visited and node in graph:
+            visited.append(node)
+            queue.extend(graph[node])
+            print(graph, visited)
+    return visited
+
+if __name__ == '__main__':
+    N, M, start = map(int, sys.stdin.readline().split())
+    input = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+
+    graph = graph_loader(N, input)
+
+    print(DFS(graph,start))
+    print(BFS(graph,start))
