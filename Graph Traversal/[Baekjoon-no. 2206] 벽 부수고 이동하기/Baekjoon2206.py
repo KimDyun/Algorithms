6e4c2f4518062898b1fb3dd77c77e3ebb04cbaf7
KimDyun <swslooser@naver.com>
2022-09-23 11:19:48 +0900

    Baekjoon2206.py update commit

--- Graph Traversal/[Baekjoon-no. 2206] 벽 부수고 이동하기/Baekjoon2206.py
+++ Graph Traversal/[Baekjoon-no. 2206] 벽 부수고 이동하기/Baekjoon2206.py
@@ -1,10 +1,41 @@
+import sys
+from collections import deque
+from itertools import combinations
+
+def bfs():
+    graph[0] = 1
+    queue = deque()
+    queue.append((0, 0))
+    
+    while queue:
+        i, c = queue.popleft()
+        x, y = i // M, i % M
+        move = [
+            i - M if 0 < x else -1,
+            i + M if x < N - 1 else -1,
+            i - 1 if 0 < y else -1,
+            i + 1 if y < M - 1 else -1
+        ]
+        for m in move:
+            if m != -1:
+                if graph[m] == 0:
+                    queue.append((m, c))
+                    graph[m] = graph[i] + 1
+                    continue
+                if graph[m] == 1 and c == 0 and m != 0:
+                    queue.append((m, 1))
+                    graph[m] = graph[i] + 1
+
+    if graph[size-1] == 0:
+        return -1
+    return graph[size-1]
+
 if __name__ == '__main__':
     N, M = map(int, sys.stdin.readline().split())
     size = N * M
     graph = [0] * size
-    for i in range(0, size, M):
-        graph[i: i + M] = [*map(int, sys.stdin.readline().split())]
+    for idx in range(0, size, M):
+        graph[idx: idx + M] = [*map(int, sys.stdin.readline().rstrip())]
 
-    answer = 0
-    bfs()
+    answer = bfs()
     print(answer)
