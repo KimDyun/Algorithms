213e8c74e352bcc8000e41f4ba2759aa4a8552cf
KimDyun <swslooser@naver.com>
2022-09-23 14:58:41 +0900

    Baekjoon2206.py update commit

--- Graph Traversal/[Baekjoon-no. 2206] 벽 부수고 이동하기/Baekjoon2206.py
+++ Graph Traversal/[Baekjoon-no. 2206] 벽 부수고 이동하기/Baekjoon2206.py
@@ -4,11 +4,15 @@ from itertools import combinations
 
 def bfs():
     graph[0] = 1
+    visit = [[0, 0] for _ in range(size)]
+    visit[0][1] = 1
     queue = deque()
-    queue.append((0, 0))
-    
+    queue.append((0, 1))
     while queue:
-        i, c = queue.popleft()
+        i, c = queue.popleft() # c : 벽을 깰 수 있는 횟수
+        if i == size - 1:
+            print(visit[i][c])
+            exit(0)
         x, y = i // M, i % M
         move = [
             i - M if 0 < x else -1,
@@ -17,25 +21,23 @@ def bfs():
             i + 1 if y < M - 1 else -1
         ]
         for m in move:
-            if m != -1:
-                if graph[m] == 0:
-                    queue.append((m, c))
-                    graph[m] = graph[i] + 1
-                    continue
-                if graph[m] == 1 and c == 0 and m != 0:
-                    queue.append((m, 1))
-                    graph[m] = graph[i] + 1
+            if m == -1:
+                continue
+            if c == 1 and graph[m] == 1:
+                queue.append((m, 0))
+                visit[m][0] = visit[i][1] + 1
+            elif graph[m] == 0 and visit[m][c] == 0:
+                queue.append((m, c))
+                visit[m][c] = visit[i][c] + 1
 
-    if graph[-1] == 0:
-        return -1
-    return graph[-1]
+    print(-1)
 
 if __name__ == '__main__':
     N, M = map(int, sys.stdin.readline().split())
+
     size = N * M
-    graph = [0] * size
+    graph = [[0] * size]
     for idx in range(0, size, M):
         graph[idx: idx + M] = [*map(int, sys.stdin.readline().rstrip())]
 
-    answer = bfs()
-    print(answer)
+    bfs()
