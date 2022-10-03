fe93a9fb5ba489440a0af4ec74b774830f354e5d
KimDyun <swslooser@naver.com>
2022-10-03 14:41:04 +0900

    Update 강의실 배정.py

--- Softeer practice/강의실 배정.py
+++ Softeer practice/강의실 배정.py
@@ -3,27 +3,42 @@ import sys
 """
     Quick sort 구현
 """
+
 def partition(arr, low, high):
-    pivot = arr[(low + high) // 2]
+    pivot = arr[(low+high)//2][1]
+
     while low <= high:
-        while arr[low] < pivot:
+        while arr[low][1] < pivot:
             low += 1
-        while arr[high] > pivot:
+        while arr[high][1] > pivot:
             high -= 1
         if low <= high:
             arr[low], arr[high] = arr[high], arr[low]
             low, high = low + 1, high - 1
     return low
 
-def qsort(arr, low, high):
+
+# Function to find the partition position
+def quickSort(arr, low, high):
     if high <= low:
         return
 
     mid = partition(arr, low, high)
-    qsort(arr, low, mid - 1)
-    qsort(arr, mid, high)
+    quickSort(arr, low, mid - 1)
+    quickSort(arr, mid, high)
 
+
+def scheduling(lect):
+    cnt = 1
+    _, ff = lect[0]
+    for s, f in lect[1:]:
+        if s >= ff:
+            cnt+=1
+            ff = f
+    print(cnt)
 
 if __name__ == "__main__":
-    N = int(sys.stdin.readline().rstrtip())
-    lectures = tuple(map(int, sys.stdin.readline().split()))
+    N = int(sys.stdin.readline().rstrip())
+    lectures = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+    quickSort(lectures, 0, N-1)
+    scheduling(lectures)
