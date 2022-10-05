d15d7d5f88b7ac745c5070604301bed6b1d5a34e
KimDyun <swslooser@naver.com>
2022-10-05 19:32:48 +0900

    금고털이.py first commit (need to update)

---
+++ Softeer practice/금고털이.py
@@ -0,0 +1,27 @@
+import sys
+
+if __name__ == "__main__":
+    W, N = map(int, sys.stdin.readline().split())
+    jews = [tuple(map(int,sys.stdin.readline().split())) for _ in range(N)]
+
+    kf = (lambda x: x[0]*x[1])
+    jews.sort(key = kf, reverse = True)
+
+    answer = 0
+    total_W, total_P = 0, 0
+
+    m, p = jews.pop(0)
+    if m < W:
+        total_W = m
+        total_P = m*p
+
+    while jews:
+        m, p = jews.pop(0)
+        if total_W + m > W:
+            break
+        total_W += m
+        total_P += m*p
+    res = W - total_W
+    total_P += res*p
+
+    print(total_P)
